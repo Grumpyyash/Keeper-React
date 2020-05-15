a477d8b7a11f3a1b99ee3ba14ad4ad133287ba1d
Yash vardhan <62200066+Grumpyyash@users.noreply.github.com>
2020-05-15 15:35:01 +0530

    Initial commit

---
+++ keeper react/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+       <NoteAddIcon /> Keeper
+        </h1>
+    </header>
+  );
+}
+
+export default Header;
